5d41a9780c018692b297809701ae95ec4c70840a
MathieuCeren <cerenmathieu@gmail.com>
2020-03-31 12:30:25 +0200

    change api to function

--- src/components/Home/Planets.js
+++ src/components/Home/Planets.js
@@ -1,19 +1,43 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// export default class Planets extends Component {
+//     state = {
+//         hasErrors: false,
+//         planets: {}
+//     };
 
-export default class Planets extends Component {
-    state = {
-        hasErrors: false,
-        planets: {}
-    };
+//     componentDidMount() {
+//         fetch("https://swapi.co/api/planets/4/")
+//             .then(res => res.json())
+//             .then(res => this.setState({ planets: res }))
+//             .catch(() => this.setState({ hasErrors: true }));
+//     }
 
-    componentDidMount() {
-        fetch("https://swapi.co/api/planets/4/")
-            .then(res => res.json())
-            .then(res => this.setState({ planets: res }))
-            .catch(() => this.setState({ hasErrors: true }));
-    }
+//     render() {
+//         return <div>{JSON.stringify(this.state.planets)}</div>;
+//     }
+// }
 
-    render() {
-        return <div>{JSON.stringify(this.state.planets)}</div>;
-    }
-}
+function Planets() {
+    const [data, setData] = useState({ hits: [] });
+    useEffect(async () => {
+        async function fetchData  ()  {
+            const result = await axios(
+                'https://hn.algolia.com/api/v1/search?query=redux',
+            ).getData();
+            setData(result.data);
+        };
+        fetchData();
+    }, []);
+    return (
+        <ul>
+            {data.hits.map(item => (
+                <li key={item.objectID}>
+                    <a href={item.url}>{item.title}</a>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default Planets;
