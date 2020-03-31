b9c11018c5584904313fadd3d6cf8a3671429c42
MathieuCeren <cerenmathieu@gmail.com>
2020-03-31 12:43:49 +0200

    planets

--- src/components/Home/Planets.js
+++ src/components/Home/Planets.js
@@ -1,43 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-// export default class Planets extends Component {
-//     state = {
-//         hasErrors: false,
-//         planets: {}
-//     };
+import React, {  Component } from 'react';
+// import axios from 'axios';
 
-//     componentDidMount() {
-//         fetch("https://swapi.co/api/planets/4/")
-//             .then(res => res.json())
-//             .then(res => this.setState({ planets: res }))
-//             .catch(() => this.setState({ hasErrors: true }));
-//     }
+export default class Planets extends Component {
+    state = {
+        hasErrors: false,
+        planets: {}
+    };
 
-//     render() {
-//         return <div>{JSON.stringify(this.state.planets)}</div>;
-//     }
-// }
+    componentDidMount() {
+        fetch("https://swapi.co/api/planets/4/")
+            .then(res => res.json())
+            .then(res => this.setState({ planets: res }))
+            .catch(() => this.setState({ hasErrors: true }));
+    }
 
-function Planets() {
-    const [data, setData] = useState({ hits: [] });
-    useEffect(async () => {
-        async function fetchData  ()  {
-            const result = await axios(
-                'https://hn.algolia.com/api/v1/search?query=redux',
-            ).getData();
-            setData(result.data);
-        };
-        fetchData();
-    }, []);
-    return (
-        <ul>
-            {data.hits.map(item => (
-                <li key={item.objectID}>
-                    <a href={item.url}>{item.title}</a>
-                </li>
-            ))}
-        </ul>
-    );
-};
+    render() {
+        return( 
+            <div>
+                {JSON.stringify(this.state.planets)}
+                <div>
+                    <h1>{this.state.planets.name}</h1>
+                    <p>{this.state.planets.rotation_period}</p>
+                </div>           
+            </div>
+        );
+    }
+}
 
-export default Planets;
+// function Planets() {
+//     const [data, setData] = useState({ hits: [] });
+//     useEffect(async () => {
+//         async function fetchData  ()  {
+//             const result = await axios(
+//                 'https://hn.algolia.com/api/v1/search?query=redux',
+//             ).getData();
+//             setData(result.data);
+//         };
+//         fetchData();
+//     }, []);
+//     return (
+//         <ul>
+//             {data.hits.map(item => (
+//                 <li key={item.objectID}>
+//                     <a href={item.url}>{item.title}</a>
+//                 </li>
+//             ))}
+//         </ul>
+//     );
+// };
+
+// export default Planets;
